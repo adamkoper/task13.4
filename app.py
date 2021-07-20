d0b01fee435baa3e224f484402cbe90c2190700e
Adam Koprowski <adamkoprowski4@gmail.com>
2021-07-20 20:47:48 +0200

    fix bugs PEP8

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from forms import BookForm
 from models import books
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "nininini"
+app.config["SECRET_KEY"] = "bububu"
+
 
 @app.route("/api/v1.0/library/", methods=["GET"])
 def books_list():
     form = BookForm()
     error = ""
     return render_template("books.html", form=form, books=books.all(), error=error)
+
 
 @app.route("/api/v1.0/library/", methods=["POST"])
 def create_book():
@@ -19,11 +21,13 @@ def create_book():
         books.create(form.data)
     return redirect(url_for('books_list'))
 
+
 @app.route("/api/v1.0/book/<int:book_id>/", methods=["GET"])
 def book_details(book_id):
     book = books.get(book_id - 1)
     form = BookForm(data=book)
     return render_template("book.html", form=form, book_id=book_id)
+
 
 @app.route("/api/v1.0/book/<int:book_id>/", methods=["POST"])
 def book_update(book_id):
@@ -32,7 +36,6 @@ def book_update(book_id):
     if form.validate_on_submit():
         books.update(book_id - 1, form.data)
     return redirect(url_for('books_list'))
-
 
 
 if __name__ == "__main__":
